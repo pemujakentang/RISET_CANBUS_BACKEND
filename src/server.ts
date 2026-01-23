8b42e3b6b8fec3b9e30f03f6e8e867aa97a9725a
FizKw <hafizhkumara.w@gmail.com>
2026-01-23 11:26:38 +0700

    change port listend

--- src/server.ts
+++ src/server.ts
@@ -232,6 +232,8 @@ app.get("/api/telemetry/history", async (req, res) => {
 
 
 const PORT = 4000;
-app.listen(PORT, () =>
-  console.log(`🚀 Backend running at http://localhost:${PORT}`)
-);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
+});
+
